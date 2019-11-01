21d09a0c4c011806465c120a7898774400baf93b
Akram Khalid <akramkhalid12@gmail.com>
2019-11-01 11:13:27 -0500

    init commit

---
+++ src/components/banner.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Banner = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      poppinShades: file(relativePath: { eq: "poppin-shades.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      diamonds: file(relativePath: { eq: "diamonds.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      orange: file(relativePath: { eq: "sexy-orange.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div className="banner">
+      <div className="container">
+        <div className="row">
+          <div className="side-image left">
+            <Img fluid={data.diamonds.childImageSharp.fluid} />
+          </div>
+          <div className="main-text">Oladimeji Odunsi</div>
+          <div className="main-image">
+            <Img fluid={data.poppinShades.childImageSharp.fluid} />
+          </div>
+          <div className="side-image right">
+            <Img fluid={data.orange.childImageSharp.fluid} />
+          </div>
+        </div>
+        <div className="scroll">
+          <span>Scroll down</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,23 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header>
+    <div className="container">
+      <div className="inner-header">
+        <div className="logo">
+          <Link to="/">ODUNSI</Link>
+        </div>
+        <div className="navigation">
+          <nav>
+            <Link to="/about">About</Link>
+            <Link to="/work">Work</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
+        </div>
+      </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
